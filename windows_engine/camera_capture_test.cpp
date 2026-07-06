9df92d4246a3853d58362e9d72164bef95711c60
ashish-karigar <karigarashish@gmail.com>
2026-07-06 12:07:35 -0400

    testing windows c++ image capture

---
+++ windows_engine/camera_capture_test.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <dshow.h>
+#include <iostream>
+
+#pragma comment(lib, "strmiids.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "oleaut32.lib")
+
+int main() {
+    std::cout << "Next phase: native capture test scaffold ready.\n";
+    std::cout << "We will now add DirectShow graph capture by device path.\n";
+    return 0;
+}
