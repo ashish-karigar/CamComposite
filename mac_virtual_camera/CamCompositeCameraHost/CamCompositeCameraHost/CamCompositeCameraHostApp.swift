c1ebd005757a9770a1c1286d4c091ae019e88b75
ashish-karigar <karigarashish@gmail.com>
2026-07-07 17:52:26 -0400

    Add macOS camera host Xcode project

---
+++ mac_virtual_camera/CamCompositeCameraHost/CamCompositeCameraHost/CamCompositeCameraHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamCompositeCameraHostApp.swift
+//  CamCompositeCameraHost
+//
+//  Created by Ashish Karigar on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CamCompositeCameraHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac_virtual_camera/CamCompositeCameraHost/CamCompositeCameraHost/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CamCompositeCameraHost
+//
+//  Created by Ashish Karigar on 7/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
